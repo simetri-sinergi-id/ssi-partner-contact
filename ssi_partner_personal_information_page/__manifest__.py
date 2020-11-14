c8fe6f6107e86fd2f40476662c75aa0db8886a91
azmimr67@gmail.com <azmimr67@gmail.com>
2020-11-14 22:02:52 +0700

    [ssi_partner_personal_information_page] 14.0.1.0.0

---
+++ ssi_partner_personal_information_page/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Contact - Personal Information Page",
+    "version": "14.0.1.0.0",
+    "website": "https://github.com/simetri-sinergi-id/ssi-partner-contact",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+}
