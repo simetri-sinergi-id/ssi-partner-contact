c8bb758d7cae124cbb4156c5d4636fa3810aa15d
azmimr67@gmail.com <azmimr67@gmail.com>
2020-11-16 10:55:09 +0700

    [ssi_partner_contact_blood_type] 14.0.1.0.0

---
+++ ssi_partner_contact_blood_type/models/res_partner.py
@@ -0,0 +1,27 @@
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    blood_type_aob = fields.Selection(
+        string="Blood Type (ABO)",
+        selection=[
+            ("a", "A"),
+            ("b", "B"),
+            ("ab", "AB"),
+            ("o", "O"),
+        ],
+    )
+    blood_type_rh = fields.Selection(
+        string="Blood Type (Rh)",
+        selection=[
+            ("+", "+"),
+            ("-", "-"),
+        ],
+    )
