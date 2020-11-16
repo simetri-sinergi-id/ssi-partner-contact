5f73880292ce4d1b0ff90548d118391934c89610
azmimr67@gmail.com <azmimr67@gmail.com>
2020-11-16 10:55:09 +0700

    [ssi_partner_contact_blood_type] 14.0.1.0.0

---
+++ ssi_partner_contact_blood_type/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Contact Blood Type",
+    "version": "14.0.1.0.0",
+    "website": "https://github.com/simetri-sinergi-id/ssi-partner-contact",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["ssi_partner_personal_information_page"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+}
