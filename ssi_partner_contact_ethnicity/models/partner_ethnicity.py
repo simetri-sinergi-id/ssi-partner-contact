195e82032e4eb15581b8f70267fc8bad3e3275e9
azmimr67@gmail.com <azmimr67@gmail.com>
2020-11-19 11:08:42 +0700

    [ssi_partner_contact_ethnicity] 14.0.1.0.0

---
+++ ssi_partner_contact_ethnicity/models/partner_ethnicity.py
@@ -0,0 +1,25 @@
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class PartnerEthnicity(models.Model):
+    _name = "partner.ethnicity"
+    _description = "Ethnicity"
+
+    name = fields.Char(
+        string="Ethnicity",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+    )
+    note = fields.Text(
+        string="Note",
+    )
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+    )
