c53443c05acd1aae01a4db4f154cc4c22734fcc7
azmimr67@gmail.com <azmimr67@gmail.com>
2020-11-19 11:07:59 +0700

    [ssi_partner_contact_religion] 14.0.1.0.0

---
+++ ssi_partner_contact_religion/models/partner_religion.py
@@ -0,0 +1,24 @@
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class PartnerReligion(models.Model):
+    _name = "partner.religion"
+    _description = "Religion"
+
+    name = fields.Char(
+        string="Religion",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+    )
+    note = fields.Text(
+        string="Note",
+    )
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+    )
