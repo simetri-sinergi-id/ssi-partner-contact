c3a7cfe5eb97c43183d5894e361b346f6f7de234
azmimr67@gmail.com <azmimr67@gmail.com>
2020-11-19 19:03:30 +0700

    [ssi_partner_emergency_contact] 15.0.1.0.0 Travis - Standarisasi

--- ssi_partner_emergency_contact/__manifest__.py
+++ ssi_partner_emergency_contact/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -10,9 +9,7 @@
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "base"
-    ],
+    "depends": ["base"],
     "data": [
         "views/res_partner_views.xml",
     ],
--- ssi_partner_emergency_contact/models/res_partner.py
+++ ssi_partner_emergency_contact/models/res_partner.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -8,14 +7,16 @@ from odoo import fields, models
 
 
 class ResPartner(models.Model):
+    _name = "res.partner"
     _inherit = "res.partner"
 
     emergency_contact_ids = fields.Many2many(
-        string='Emergency Contacts',
-        comodel_name='res.partner',
-        relation='rel_partner_emergency_contact',
-        column1='partner_id',
-        column2='emergency_contact_id',
+        string="Emergency Contacts",
+        comodel_name="res.partner",
+        relation="rel_partner_emergency_contact",
+        column1="partner_id",
+        column2="emergency_contact_id",
         domain=[
-            ('is_company', '=', False),
-        ])
+            ("is_company", "=", False),
+        ],
+    )
