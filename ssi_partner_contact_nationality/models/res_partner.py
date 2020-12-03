220d3d8f4f043177f20bbe2195290e73d305a83f
azmimr67@gmail.com <azmimr67@gmail.com>
2020-12-03 12:09:46 +0700

    [ssi_partner_contact_nationality] 14.0.1.0.0

---
+++ ssi_partner_contact_nationality/models/res_partner.py
@@ -0,0 +1,16 @@
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _name = "res.partner"
+    _inherit = "res.partner"
+
+    nationality_id = fields.Many2one(
+        string="Nationality",
+        comodel_name="res.country",
+    )
